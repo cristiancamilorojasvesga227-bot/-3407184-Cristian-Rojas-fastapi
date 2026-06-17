60707603a00ee827f95353446c429a9f9e704c62
CristianRojas <cristian.camilorojasvesga227@gmail.com>
2026-06-17 14:55:01 -0500

    Delete proyecto_clientes directory

--- proyecto_clientes/models/transacciones.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from ..database import Base
-
-class Transaccion(Base):
-    __tablename__ = "transacciones"
-
-    id = Column(Integer, primary_key=True, index=True)
-    descripcion = Column(String, nullable=False)
-    factura_id = Column(Integer, ForeignKey("facturas.id"), nullable=False)
