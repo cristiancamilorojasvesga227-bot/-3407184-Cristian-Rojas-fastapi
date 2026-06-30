b3f91e45f385bf55a2531e23bd2c7c358e665664
CristianRojas <cristian.camilorojasvesga227@gmail.com>
2026-06-30 18:54:24 -0500

    separación de modelos, y el id para la creación

--- main.py
+++ main.py
@@ -1,37 +1,30 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from modelos.clientes import Cliente , ClienteCrear
 
 app = FastAPI()
-
-
-# Creaar el modelo clientes(id, nombre, email, descripcion)
-class Cliente(BaseModel):
-    id: int
-    nombre: str
-    email: str
-    descripcion: str
 
 
 lista_clientes: list[Cliente] = []
 
 
 # endpoint, para obtener o listar todos los clientes
-@app.get("/clientes")
+@app.get("/clientes", response_model=list[Cliente])
 def listar_clientes():
     return lista_clientes
 
 
 # endpoint, para obtener o listar un solo cliente de la lista
-@app.get("/clientes/{cliente_id}")
+@app.get("/clientes/{cliente_id}", response_model=Cliente)
 def listar_cliente(cliente_id: int):
-    # recorrer (variable) obj_cliente: Cliente
+    # recorrer la lista_clientes
     for i, obj_cliente in enumerate(lista_clientes):
         if obj_cliente.get("id") == cliente_id:
             return obj_cliente
-        
+
 
 # endpoint, para crear un cliente, y agregar a la lista
-@app.post("/clientes")
-def crear_cliente(datos_cliente: Cliente):
-    lista_clientes.append(datos_cliente)
-    return datos_cliente
+@app.post("/clientes", response_model=Cliente)
+def crear_cliente(datos_cliente: ClienteCrear):
+    cliente_val = Cliente.model_validate(datos_cliente.model_dump())
+    lista_clientes.append(cliente_val)
+    return cliente_val
